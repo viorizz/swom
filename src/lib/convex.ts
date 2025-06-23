3710396d0243ab61a2473762af33c9febac39d0c
Guillaume <guillaume@rllnd.com>
2025-06-23 11:54:34 +0000

    Removed unused ConvexProvider.

--- src/lib/convex.ts
+++ src/lib/convex.ts
@@ -1,5 +1,4 @@
-// src/lib/convex.ts
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
